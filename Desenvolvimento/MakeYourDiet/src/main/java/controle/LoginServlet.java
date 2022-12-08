1833996582a99fa1f8d20f5742812b61a655029f
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2022-12-07 23:13:27 -0300

    Inicio da preparação back-end

---
+++ Desenvolvimento/MakeYourDiet/src/main/java/controle/LoginServlet.java
@@ -0,0 +1,57 @@
+package controle;
+
+import jakarta.servlet.http.HttpServlet;
+import modelo.Usuario;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UsuarioDAO;
+
+/**
+ * Servlet implementation class Login
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("usuario");
+		String senha = request.getParameter("senha");
+		Usuario u = null;
+		
+		UsuarioDAO dao = new UsuarioDAO();
+		
+		u = dao.logarUsuario(email, senha);
+		
+		/*
+		if(u != null) {
+			response.sendRedirect("menu.jsp");
+			
+		}else {
+			response.sendRedirect("erro.jsp");
+		}
+		
+	}*/
+
+}
---
+++ Desenvolvimento/MakeYourDiet/src/main/java/dao/UsuarioDAO.java
@@ -0,0 +1,45 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import modelo.Usuario;
+
+public class UsuarioDAO {
+	Connection cnx = null;
+	
+	public Usuario logarUsuario(String login, String senha) {
+		Usuario usuario = null; 
+		ResultSet rs = null;
+		
+		cnx = DAO.createConnection();
+		String sql = "SELECT * FROM tb_usuario WHERE email = ? AND senha = ?";
+		
+		try {
+			PreparedStatement ps = cnx.prepareStatement(sql);
+			
+			ps.setString(1, email);
+			ps.setString(2, senha);
+			
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				usuario = new Usuario();
+				
+				usuario.setId(rs.getInt("id"));
+				usuario.setNomeCompleto(rs.getString("nomeCompleto"));
+				usuario.setEmail(rs.getString("email"));
+				usuario.setSenha(rs.getString("senha"));
+				usuario.setDataNasc(rs.getString("dataNasc"));
+				
+			}
+			
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return usuario;
+	}
+}
