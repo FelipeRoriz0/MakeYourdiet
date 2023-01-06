529997b872521a9e6396db2cbc09a189299ff414
Felipe Roriz <f.roriz@hotmail.com>
2023-01-05 22:31:25 -0300

    Carga da tabela alimento incompleto

---
+++ Documentacao/Script_BD/tb_alimento/script_laticínios.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Leite', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Queijo', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Requeijão', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Manteiga', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Creme de leite', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Iogurte', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Doce de leite', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Leite condensado', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Sorvete', 3);
+INSERT INTO tb_alimento(nome, id_TipoAlimento) VALUES('Margarina', 3);
+
