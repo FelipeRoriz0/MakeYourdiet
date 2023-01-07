d120b043c25d3bec52fe471f9d821fdf3c236cc2
Felipe Roriz <f.roriz@hotmail.com>
2023-01-07 18:00:21 -0300

    Primeiras receitas de emagrecimento no banco

---
+++ Documentacao/Script_BD/tb_receitas/script_receitas_emagrecimento.sql
@@ -0,0 +1,21 @@
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco detox de couve com abacaxi ', '3 folhas de couve, 3 rodelas de gengibre,2 fatias de abacaxi congelado, Chia a gosto, 200 ml de água (ou água de coco),Suco de 1/2 limão', 'Reúna todos os ingredientes, Coloque todos os ingredientes no liquidificador e bata por cerca de 5 minutos, Suco detox de couve com abacaxi, Transfira o suco para um copo e, caso queira, adoce com mel. ', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco verde com ora-pro-nóbis', '4 maçãs, 200 ml de água, 6 folhas de azedinha, 8 folhas de ora-pro-nóbis, 1 colher de sobremesa de gengibre fresco picado', 'Bata todos os ingredientes no liquidificador até virar um suco bem grosso, Coe no saco de voil ou passe por uma peneira bem fina, Está pronto para beber! . ', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco verde digestivo ', '1 fatia de abacaxi, Suco de 1 limão, Folhas de hortelã a gosto, 1/2 folha de couve, 200 ml de água de coco (natural)', 'Separe e corte os ingredientes sólidos em pedaços menores, para triturar melhor, Coloque todos os ingredientes no liquidificador e bata até ficar homogêneo. Passe para um copo e coloque gelo. Pode usar uma peneira. Agora é só servir. Bom apetite. ', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco de maçã com gengibre  ', '4 maçãs cortadas em pedaços (pode ser gala, fuji ou red), Cerca de 300 ml de água, 1 pedaço de gengibre descascado', 'Coloque no liquidificador, a maçã, a água e bata até ficar uniforme., Adicione o gengibre e bata por mais 10 segundos., Agora é só servir. Bom apetite ', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco detox de beterraba   ', '300 ml de água gelada, 1 beterraba pequena, crua e em pedaços, Sumo de 1 limão, 1 colher de chá de mel (opcional), ' , ' Em um liquidificador, coloque todos os ingredientes e bata até misturar bem. Transfira para um copo e adicione gelo. Agora é só servir. Bom apetite.', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco vermelho pré-treino', '100 gramas de beterraba, 100 gramas de framboesa, 20 gramas de biomassa de banana verde, 200 ml água', '    Em um liquidificador, coloque a beterraba sem casca e cortada em cubos, a framboesa, a biomassa, a água gelada e bata muito bem. Sirva bem geladinho. Aproveite.', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco de laranja e cenoura ', 'Suco de 12 laranjas, 1 cenoura (com casca), 1 colher de sopa de açúcar', ' Coloque tudo no liquidificador e bata até ficar homogêneo. Agora é só servir. Bom apetite.', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco diurético de melancia ', '1 rodela de gengibre, 1 fatia de melancia, 200 ml de água de coco', 'No liquidificador, bata o gengibre com a melancia e a água de coco até obter um líquido homogêneo. Sirva sem coar e adoçar logo em seguida. Aproveite sua bebida..', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Suco de capim-cidreira ', '1 xícara de chá de capim-cidreira picado, 1/2 xícara de chá de folhas de hortelã, 200 ml de água gelada, Suco de 1/2 limão, 1 pedaço pequeno de gengibre', ' Coloque todos os ingredientes no liquidificador e bata até ficar homogêneo. Coe o suco e transfira para uma jarra. Acrescente e gelo no copo e sirva! Aproveite sua bebida.', 1);
+
+INSERT INTO tb_receitas(nomeReceita, receita, preparo, id_tipoReceita) VALUES('Chá-verde com abacaxi e laranja ', '1 litro de água filtrada, 2 saquinhos de chá-verde, 1/2 abacaxi cortado em pedaços, Um pedaço pequeno de gengibre, Folhas de hortelã, 1/2 laranja-lima cortada em rodelas, Mel a gosto, Gelo a gosto', 'Ferva a água em uma leiteira. Assim que ferver, desligue o fogo, adicione os saquinhos de chá-verde, tampe e deixe descansar por cerca de 30 minutos. Acrescente o restante dos ingredientes, misture bem e leve para a geladeira até gelar. Agora é só servir! Bom apetite.', 1);
+
+
